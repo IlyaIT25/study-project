2ca050eca1ae0ecfaf22f8443763568517836b71
Ilya <polyak.i.2506@gmail.com>
2024-10-17 19:51:52 +0300

    HW3.2

---
+++ HomeWork3.2.py
@@ -0,0 +1,17 @@
+numbers_1 = [12, 10, 8, 4, 6]
+print(numbers_1)
+x = numbers_1.pop(4)
+numbers_1.insert(0,x)
+print(numbers_1)
+
+numbers_3 = [12, 10, 8, 4, 6, 22, 13]
+print(numbers_3)
+x = numbers_3.pop(6)
+numbers_3.insert(0,x)
+print(numbers_3)
+
+numbers_2 = [12]
+print(numbers_2)
+x = numbers_2.pop(0)
+numbers_2.insert(0,x)
+print(numbers_2)
